a3016e5867075c7d73d8ea72336a6ce598da90ba
Jeongwon <picasomati@gmail.com>
2026-08-24 12:15:49 +0900

    Update header.js

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -14,7 +14,7 @@ function renderHeader() {
             </button>
           </li>
           <li>
-            <a href="/est_fe13_2nd_project/" class="site__nav__logo text-headline-small" id="site-nav-label">ROUNZ</a>
+            <a href="/" class="site__nav__logo text-headline-small" id="site-nav-label">ROUNZ</a>
           </li>
         </ul>
         <label for="theme" class="visually-hidden">화면 테마 변경</label>
@@ -25,13 +25,13 @@ function renderHeader() {
         </select>
         <ul class="site__nav__actions" role="list">
           <li>
-            <a href="/est_fe13_2nd_project/login.html">
+            <a href="/login.html">
               <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-round-icon lucide-user-round"><circle cx="12" cy="8" r="5"/><path d="M20 21a8 8 0 0 0-16 0"/></svg>
               <span class="visually-hidden">프로필</span>
             </a>
           </li>
           <li>
-            <a href="/est_fe13_2nd_project/cart.html" class="pile">
+            <a href="/cart.html" class="pile">
               <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-cart-icon lucide-shopping-cart"><circle cx="8" cy="21" r="1"/><circle cx="19" cy="21" r="1"/><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"/></svg>
               <span class="visually-hidden">장바구니</span>
               <span class="cart__counter badge">1</span>
@@ -44,10 +44,10 @@ function renderHeader() {
             <span class="visually-hidden">메뉴 닫기</span>
           </button>
           <ul role="list">
-            <li><a href="/est_fe13_2nd_project/productList.html?action=showSunglasses">선글라스</a></li>
-            <li><a href="/est_fe13_2nd_project/productList.html?action=showLens">렌즈</a></li>
-            <li><a href="/est_fe13_2nd_project/productList.html?action=showAccessory">액세서리</a></li>
-            <li><a href="/est_fe13_2nd_project/productList.html?action=showEyewear">아이웨어</a></li>
+            <li><a href="/productList.html?action=showSunglasses">선글라스</a></li>
+            <li><a href="/productList.html?action=showLens">렌즈</a></li>
+            <li><a href="/productList.html?action=showAccessory">액세서리</a></li>
+            <li><a href="/productList.html?action=showEyewear">아이웨어</a></li>
 				  </ul>
         </div>
       </nav>
